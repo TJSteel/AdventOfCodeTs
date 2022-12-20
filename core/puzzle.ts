4493749c99253a04c15d766e6b96a6bf418c708a
TJSteel <jaysteel78@msn.com>
2022-12-20 00:03:50 +0000

    added additional settings to main puzzle

--- core/puzzle.ts
+++ core/puzzle.ts
@@ -2,15 +2,25 @@ import { readFileSync } from 'fs';
 import { PuzzleStatus } from './enums';
 import { logger } from './utils';
 
+interface AdditionalPuzzleSettings {
+  testInputOnly?: boolean;
+  lowAnswers?: PuzzleAnswer;
+  highAnswers?: PuzzleAnswer;
+}
+
+interface PuzzleAnswer {
+  test1?: any;
+  main1?: any;
+  test2?: any;
+  main2?: any;
+}
+
 export abstract class AbstractPuzzle {
   private year: string;
   private day: string;
-  private answers!: {
-    test1: any;
-    main1: any;
-    test2: any;
-    main2: any;
-  };
+  private answers!: PuzzleAnswer;
+  private lowAnswers?: PuzzleAnswer;
+  private highAnswers?: PuzzleAnswer;
   private testInputOnly: boolean = false;
   protected input: any[];
   public status: PuzzleStatus;
@@ -30,14 +40,16 @@ export abstract class AbstractPuzzle {
     this.status = status;
   }
 
-  public setAnswers(test1: any, main1: any, test2: any, main2: any, testInputOnly: boolean = false): void {
+  public setAnswers(test1: any, main1: any, test2: any, main2: any, settings?: AdditionalPuzzleSettings): void {
     this.answers = {
       test1: test1,
       main1: main1,
       test2: test2,
       main2: main2,
     };
-    this.testInputOnly = testInputOnly;
+    this.testInputOnly = settings?.testInputOnly ? true : false;
+    this.lowAnswers = settings?.lowAnswers;
+    this.highAnswers = settings?.highAnswers;
   }
 
   public parseInput(): void {
@@ -66,7 +78,14 @@ export abstract class AbstractPuzzle {
     throw new Error('calculateAnswer2 not implemented!');
   }
 
-  private runner = (name: string, getInput: Function, calculateAnswer: Function, answer: number): boolean => {
+  private runner = (
+    name: string,
+    getInput: Function,
+    calculateAnswer: Function,
+    testKey: keyof PuzzleAnswer
+  ): boolean => {
+    const answer = this.answers[testKey];
+
     this.isTest = name.includes('Test');
     if (answer === -1) {
       logger.logColor(
@@ -87,26 +106,33 @@ export abstract class AbstractPuzzle {
     }
     let end = process.hrtime(start);
 
-    logger.log(
-      `${logger.getColor(`${this.year} day ${this.day} ${name} answer: `, logger.color.CYAN)}${
-        calculatedAnswer === answer
-          ? logger.getColor(calculatedAnswer, logger.color.GREEN)
-          : logger.getColor(`${calculatedAnswer} != ${answer}`, logger.color.RED)
-      }`
-    );
+    let answerString = ``;
+    if (calculatedAnswer === answer) {
+      answerString = logger.getColor(calculatedAnswer, logger.color.GREEN);
+    } else if (this.lowAnswers && this.lowAnswers[testKey] && calculatedAnswer <= this.lowAnswers[testKey]) {
+      answerString = logger.getColor(`${calculatedAnswer} is too low`, logger.color.RED);
+    } else if (this.highAnswers && this.highAnswers[testKey] && calculatedAnswer >= this.highAnswers[testKey]) {
+      answerString = logger.getColor(`${calculatedAnswer} is too high`, logger.color.RED);
+    } else if ((this.lowAnswers && this.lowAnswers[testKey]) || (this.highAnswers && this.highAnswers[testKey])) {
+      answerString = logger.getColor(`${calculatedAnswer} is a potential answer`, logger.color.GREEN);
+    } else {
+      answerString = logger.getColor(`${calculatedAnswer} != ${answer}`, logger.color.RED);
+    }
+
+    logger.log(`${logger.getColor(`${this.year} day ${this.day} ${name} answer: `, logger.color.CYAN)}${answerString}`);
     logger.logColor(`${this.year} day ${this.day} ${name} took: ${end[0]}s ${end[1] / 1000000}ms`, logger.color.CYAN);
     return calculatedAnswer === answer;
   };
 
   public run = (): number => {
     let errors = 0;
-    errors += this.runner(`Test 1`, this.getTestInput, this.calculateAnswer1, this.answers.test1) ? 0 : 1;
+    errors += this.runner(`Test 1`, this.getTestInput, this.calculateAnswer1, 'test1') ? 0 : 1;
     if (!this.testInputOnly) {
-      errors += this.runner(`Main 1`, this.getMainInput, this.calculateAnswer1, this.answers.main1) ? 0 : 1;
+      errors += this.runner(`Main 1`, this.getMainInput, this.calculateAnswer1, 'main1') ? 0 : 1;
     }
-    errors += this.runner(`Test 2`, this.getTestInput, this.calculateAnswer2, this.answers.test2) ? 0 : 1;
+    errors += this.runner(`Test 2`, this.getTestInput, this.calculateAnswer2, 'test2') ? 0 : 1;
     if (!this.testInputOnly) {
-      errors += this.runner(`Main 2`, this.getMainInput, this.calculateAnswer2, this.answers.main2) ? 0 : 1;
+      errors += this.runner(`Main 2`, this.getMainInput, this.calculateAnswer2, 'main2') ? 0 : 1;
     }
     return errors;
   };
