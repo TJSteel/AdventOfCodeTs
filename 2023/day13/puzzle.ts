7c35860b561c4d494c2aed6eb957e373bbff05c5
TJSteel <jaysteel78@msn.com>
2023-12-13 10:48:42 +0000

    2023 day 13 part 2

--- 2023/day13/puzzle.ts
+++ 2023/day13/puzzle.ts
@@ -2,7 +2,7 @@ import { Array2d } from '../../core/array2d';
 import { PuzzleStatus } from '../../core/enums';
 import { AbstractPuzzle } from '../../core/puzzle';
 
-const getHorizontalValue = (pattern: Array2d<string>): number => {
+const getHorizontalValue = (pattern: Array2d<string>, old: number = 0): number => {
   const rows = pattern.data.map((v) => v.join(''));
   outer: for (let i = 0; i < pattern.getHeight() - 1; i++) {
     let offset = 0;
@@ -13,7 +13,11 @@ const getHorizontalValue = (pattern: Array2d<string>): number => {
       let afterRow = rows[after];
 
       if (!beforeRow || !afterRow) {
-        return i + 1;
+        const value = i + 1;
+        if (value != old) {
+          return value;
+        }
+        continue outer;
       }
 
       if (beforeRow != afterRow) {
@@ -25,15 +29,15 @@ const getHorizontalValue = (pattern: Array2d<string>): number => {
   return 0;
 };
 
-const getVerticalValue = (pattern: Array2d<string>): number => {
+const getVerticalValue = (pattern: Array2d<string>, old: number = 0): number => {
   const rotated: Array2d<string> = pattern.copy();
   rotated.rotateClockwise();
-  return getHorizontalValue(rotated);
+  return getHorizontalValue(rotated, old);
 };
 
 class Puzzle extends AbstractPuzzle {
   setAnswers(): void {
-    super.setAnswers(405, 34889, 0, 0);
+    super.setAnswers(405, 34889, 400, 34224, { lowAnswers: { main2: [29268, 23795] } });
   }
 
   parseInput(): void {
@@ -59,9 +63,30 @@ class Puzzle extends AbstractPuzzle {
 
   calculateAnswer2 = (): number => {
     let answer = 0;
-
+    for (let i = 0; i < this.input.length; i++) {
+      const input = this.input[i];
+      const pattern: Array2d<string> = input;
+      let horizontalValue = getHorizontalValue(pattern);
+      let verticalValue = getVerticalValue(pattern);
+      let newHorizontalValue = 0;
+      let newVerticalValue = 0;
+      for (const cell of pattern) {
+        const copyPattern = pattern.copy();
+        copyPattern.setCell(cell.coord, copyPattern.getCell(cell.coord) == '.' ? '#' : '.');
+        let vertical = getVerticalValue(copyPattern, verticalValue);
+        let horizontal = getHorizontalValue(copyPattern, horizontalValue);
+        if (vertical > 0) {
+          newVerticalValue = vertical;
+        }
+        if (horizontal > 0) {
+          newHorizontalValue = horizontal;
+        }
+      }
+      answer += newHorizontalValue * 100;
+      answer += newVerticalValue;
+    }
     return answer;
   };
 }
 
-export const puzzle = new Puzzle('2023', '13', PuzzleStatus.IN_PROGRESS);
+export const puzzle = new Puzzle('2023', '13', PuzzleStatus.COMPLETE);
