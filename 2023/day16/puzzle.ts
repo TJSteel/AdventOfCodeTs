f542042a8316be37d0c98f5e0d69d2661f736f90
TJSteel <jaysteel78@msn.com>
2023-12-16 09:13:58 +0000

    2023 day 16 part 2

--- 2023/day16/puzzle.ts
+++ 2023/day16/puzzle.ts
@@ -13,11 +13,81 @@ interface Queue {
   coord: Coordinate2d;
   direction: Coordinate2d;
 }
+
+const getEnergisedCells = (map: Array2d<string>, start: Coordinate2d, startDirection: Coordinate2d): number => {
+  const visited: Set<string> = new Set();
+  const energised: Set<string> = new Set();
+  const queue: Queue[] = [{ coord: start, direction: startDirection }];
+  while (queue.length > 0) {
+    const current: Queue = queue.pop()!;
+    visited.add(`${current.coord.toString()},${current.direction.toString()}`);
+    energised.add(current.coord.toString());
+
+    const cell = map.getCell(current.coord);
+    const nextDirections: Coordinate2d[] = [];
+    switch (cell) {
+      case '.':
+        nextDirections.push(current.direction);
+        break;
+      case '/':
+        if (current.direction.equals(directions.LEFT)) {
+          nextDirections.push(directions.DOWN);
+        } else if (current.direction.equals(directions.RIGHT)) {
+          nextDirections.push(directions.UP);
+        } else if (current.direction.equals(directions.UP)) {
+          nextDirections.push(directions.RIGHT);
+        } else if (current.direction.equals(directions.DOWN)) {
+          nextDirections.push(directions.LEFT);
+        }
+        break;
+      case '\\':
+        if (current.direction.equals(directions.LEFT)) {
+          nextDirections.push(directions.UP);
+        } else if (current.direction.equals(directions.RIGHT)) {
+          nextDirections.push(directions.DOWN);
+        } else if (current.direction.equals(directions.UP)) {
+          nextDirections.push(directions.LEFT);
+        } else if (current.direction.equals(directions.DOWN)) {
+          nextDirections.push(directions.RIGHT);
+        }
+        break;
+      case '|':
+        if (current.direction.equals(directions.LEFT) || current.direction.equals(directions.RIGHT)) {
+          nextDirections.push(directions.UP);
+          nextDirections.push(directions.DOWN);
+        } else {
+          nextDirections.push(current.direction);
+        }
+        break;
+      case '-':
+        if (current.direction.equals(directions.UP) || current.direction.equals(directions.DOWN)) {
+          nextDirections.push(directions.LEFT);
+          nextDirections.push(directions.RIGHT);
+        } else {
+          nextDirections.push(current.direction);
+        }
+        break;
+    }
+
+    for (const direction of nextDirections) {
+      const coord = current.coord.copy().add(direction);
+      if (!map.inRange(coord)) {
+        continue;
+      }
+      const key = `${coord.toString()},${direction.toString()}`;
+      if (!visited.has(key)) {
+        queue.push({ coord, direction });
+      }
+    }
+  }
+
+  return energised.size;
+};
 class Puzzle extends AbstractPuzzle {
   map: Array2d<string> = new Array2d();
 
   setAnswers(): void {
-    super.setAnswers(46, 8323, 0, 0);
+    super.setAnswers(46, 8323, 51, 8491);
   }
 
   parseInput(): void {
@@ -26,81 +96,39 @@ class Puzzle extends AbstractPuzzle {
   }
 
   calculateAnswer1 = (): number => {
-    const visited: Set<string> = new Set();
-    const energised: Set<string> = new Set();
-    const start = new Coordinate2d(0, 0);
-    const queue: Queue[] = [{ coord: start, direction: directions.RIGHT }];
-    while (queue.length > 0) {
-      const current: Queue = queue.pop()!;
-      visited.add(`${current.coord.toString()},${current.direction.toString()}`);
-      energised.add(current.coord.toString());
-
-      const cell = this.map.getCell(current.coord);
-      const nextDirections: Coordinate2d[] = [];
-      switch (cell) {
-        case '.':
-          nextDirections.push(current.direction);
-          break;
-        case '/':
-          if (current.direction.equals(directions.LEFT)) {
-            nextDirections.push(directions.DOWN);
-          } else if (current.direction.equals(directions.RIGHT)) {
-            nextDirections.push(directions.UP);
-          } else if (current.direction.equals(directions.UP)) {
-            nextDirections.push(directions.RIGHT);
-          } else if (current.direction.equals(directions.DOWN)) {
-            nextDirections.push(directions.LEFT);
-          }
-          break;
-        case '\\':
-          if (current.direction.equals(directions.LEFT)) {
-            nextDirections.push(directions.UP);
-          } else if (current.direction.equals(directions.RIGHT)) {
-            nextDirections.push(directions.DOWN);
-          } else if (current.direction.equals(directions.UP)) {
-            nextDirections.push(directions.LEFT);
-          } else if (current.direction.equals(directions.DOWN)) {
-            nextDirections.push(directions.RIGHT);
-          }
-          break;
-        case '|':
-          if (current.direction.equals(directions.LEFT) || current.direction.equals(directions.RIGHT)) {
-            nextDirections.push(directions.UP);
-            nextDirections.push(directions.DOWN);
-          } else {
-            nextDirections.push(current.direction);
-          }
-          break;
-        case '-':
-          if (current.direction.equals(directions.UP) || current.direction.equals(directions.DOWN)) {
-            nextDirections.push(directions.LEFT);
-            nextDirections.push(directions.RIGHT);
-          } else {
-            nextDirections.push(current.direction);
-          }
-          break;
-      }
-
-      for (const direction of nextDirections) {
-        const coord = current.coord.copy().add(direction);
-        if (!this.map.inRange(coord)) {
-          continue;
-        }
-        const key = `${coord.toString()},${direction.toString()}`;
-        if (!visited.has(key)) {
-          queue.push({ coord, direction });
-        }
-      }
-    }
-
-    return energised.size;
+    return getEnergisedCells(this.map, new Coordinate2d(0, 0), directions.RIGHT);
   };
 
   calculateAnswer2 = (): number => {
-    let answer = 0;
+    let maxEnergised = 0;
 
-    return answer;
+    for (let x = 0; x < this.map.getWidth(); x++) {
+      let energised = getEnergisedCells(this.map, new Coordinate2d(x, 0), directions.DOWN);
+      if (energised > maxEnergised) {
+        maxEnergised = energised;
+      }
+      energised = getEnergisedCells(this.map, new Coordinate2d(x, this.map.getHeight() - 1), directions.UP);
+      if (energised > maxEnergised) {
+        maxEnergised = energised;
+      }
+    }
+    for (let y = 0; y < this.map.getHeight(); y++) {
+      let energised = getEnergisedCells(this.map, new Coordinate2d(0, y), directions.RIGHT);
+      if (energised > maxEnergised) {
+        maxEnergised = energised;
+      }
+      energised = getEnergisedCells(this.map, new Coordinate2d(this.map.getWidth() - 1, y), directions.LEFT);
+      if (energised > maxEnergised) {
+        maxEnergised = energised;
+      }
+    }
+
+    return maxEnergised;
   };
 }
 
-export const puzzle = new Puzzle('2023', '16', PuzzleStatus.IN_PROGRESS);
+// This can be made faster using memoization as we're checking the same paths multiple times
+// eg look at the main input cell x: 1, y: 2, val: |
+// we're checking the downwards path at least twice, one from starting above, another from starting from it's left
+// using a recursive function to get energized cells with a directional memo map should be faster
+export const puzzle = new Puzzle('2023', '16', PuzzleStatus.INEFFICIENT);
