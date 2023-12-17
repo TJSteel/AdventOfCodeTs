a3867fd56581594a8e6b8a99a90b0226d3895d5b
TJSteel <jaysteel78@msn.com>
2023-12-17 12:28:20 +0000

    2023 day 17 part 2

--- 2023/day17/puzzle.ts
+++ 2023/day17/puzzle.ts
@@ -14,7 +14,7 @@ class Puzzle extends AbstractPuzzle {
   map: Array2d<number> = new Array2d();
 
   setAnswers(): void {
-    super.setAnswers(102, 1023, 0, 0);
+    super.setAnswers(102, 1023, 94, 1165);
   }
 
   parseInput(): void {
@@ -92,10 +92,82 @@ class Puzzle extends AbstractPuzzle {
   };
 
   calculateAnswer2 = (): number => {
-    let answer = 0;
+    const startCoord = new Coordinate2d(0, 0);
+    const endCoord = new Coordinate2d(this.map.getWidth() - 1, this.map.getHeight() - 1);
+    let lowest = startCoord.manhattanDistance(endCoord) * 9;
 
-    return answer;
+    const queue: Node[] = [];
+
+    const visited: Set<string> = new Set();
+    for (const direction of Array2d.neighboursAdjacent) {
+      const nextCoord = startCoord.copy();
+      let cost = 0;
+      for (let length = 1; length <= 10; length++) {
+        nextCoord.add(direction);
+        if (this.map.inRange(nextCoord)) {
+          cost += this.map.getCell(nextCoord);
+          if (length < 4) {
+            continue;
+          }
+          visited.add(`${nextCoord.toString()}:${direction.toString()}:${length}`);
+          queue.push({
+            coord: nextCoord.copy(),
+            cost,
+            direction,
+            length,
+          });
+        }
+      }
+    }
+
+    while (queue.length > 0) {
+      queue.sort((a, b) => b.cost - a.cost);
+      const current: Node = queue.pop()!;
+      if (current.cost + current.coord.manhattanDistance(endCoord) >= lowest) {
+        continue;
+      }
+
+      if (current.coord.equals(endCoord)) {
+        if (current.cost < lowest) {
+          lowest = current.cost;
+        }
+        continue;
+      }
+
+      for (const direction of Array2d.neighboursAdjacent) {
+        if (Math.abs(direction.x) == Math.abs(current.direction.x)) {
+          continue;
+        }
+        const nextCoord = current.coord.copy();
+        let cost = current.cost;
+        for (let length = 1; length <= 10; length++) {
+          nextCoord.add(direction);
+          if (!this.map.inRange(nextCoord)) {
+            break;
+          }
+          cost += this.map.getCell(nextCoord);
+          if (length < 4) {
+            continue;
+          }
+          const visitedKey = `${nextCoord.toString()}:${current.direction.toString()}:${length}`;
+          if (!visited.has(visitedKey)) {
+            visited.add(visitedKey);
+            queue.push({
+              coord: nextCoord.copy(),
+              cost,
+              direction,
+              length,
+            });
+          }
+        }
+      }
+    }
+
+    return lowest;
   };
 }
 
-export const puzzle = new Puzzle('2023', '17', PuzzleStatus.IN_PROGRESS);
+// I think initializing a NodeMap with a vertical / horizontal option for each cell,
+// then adding paths between each node with a weight of their value,
+// then running a dijkstra algorithm to find the best path will be much faster
+export const puzzle = new Puzzle('2023', '17', PuzzleStatus.INEFFICIENT);
